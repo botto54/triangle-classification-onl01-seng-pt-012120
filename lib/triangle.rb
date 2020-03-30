74cc4d15dc9bb1068d87688e17107e3348145d4f
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-30 07:45:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,30 +6,33 @@ class Triangle
     @triangle_sides << side3
   end
   
-  def kind
-    if @triangle_sides.uniq.length == 1
-      return :equilateral
-    elsif @triangle_sides.uniq.length == 2
-      return :isosceles
-    else 
-      return :scalene
-    end
+  def valid?
+    sum_1_2 = @triangle_sides[0] + @triangle_sides[1]
+    sum_1_3 = @triangle_sides[0] + @triangle_sides[2]
+    sum_2_3 = @triangle_sides[1] + @triangle_sides[2]
     
+    if (@triangle_sides.none? {|side| side <= 0}) &&
+      (sum_1_2 > @triangle_sides[2] && sum_1_3 > @triangle_sides[1] && sum_2_3 > @triangle_sides[0])
+      return true
+    else
+      return false
+    end
+  end
+  
+  def kind
+    if valid?
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else 
+        return :scalene
+      end
+    else
+      riase TriangleError
+    end
   end
   
   class TriangleError < StandardError
   end
 end
-
-# if valid?
-#       if @triangle_sides.uniq.length == 1
-#         return :equilateral
-#       elsif @triangle_sides.uniq.length == 2
-#         return :isosceles
-#       else
-#         return :scalene
-#       end
-#     else
-#       raise TriangleError
-#     end
-#   end
