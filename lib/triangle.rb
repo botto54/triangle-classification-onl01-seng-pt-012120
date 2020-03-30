07e05c72325a588d8546b0658a8196fec43f08d0
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-30 07:29:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,10 @@
 class Triangle
   def initialize(side1, side2, side3)
-    @trianglesides = []
-    @
+    @triangle_sides = []
+    @triangle_sides << side1
+    @triangle_sides << side2
+    @triangle_sides << side3
+  end
   
   
   class TriangleError < StandardError
